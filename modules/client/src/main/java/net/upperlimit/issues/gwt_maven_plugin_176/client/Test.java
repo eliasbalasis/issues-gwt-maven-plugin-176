33193ded6a449118030b9d554623acf6e37cb3f6
eliasbalasis <eliasbalasis@gmail.com>
2025-04-03 12:21:22 +0100

    war

---
+++ modules/client/src/main/java/net/upperlimit/issues/gwt_maven_plugin_176/client/Test.java
@@ -0,0 +1,10 @@
+package net.upperlimit.issues.gwt_maven_plugin_176.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Test implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+    }
+}
